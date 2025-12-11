dafaabdd7c92b90cc6ad31e1bd414578da999a23
azizbek-egamov <azizegamov64@gmail.com>
2025-12-11 09:34:27 +0500

    uploaded

---
+++ bot/handler/private.py
@@ -0,0 +1,18 @@
+import os
+import django
+from aiogram import Router, F, Bot, types
+from aiogram.filters import StateFilter, CommandStart, Command
+from aiogram.fsm.context import FSMContext
+from aiogram.types import Message, CallbackQuery, FSInputFile, InputMediaPhoto, InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+from asgiref.sync import sync_to_async
+
+# Django setup
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+django.setup()
+
+from main.models import *
+from django.db.models import IntegerField
+from django.db.models.functions import Cast
+
+router = Router()
